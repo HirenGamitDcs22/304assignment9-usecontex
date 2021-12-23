daa5a7435b6157ebfd2ace06807c547bbed83051
Gamit Hiren <91384225+HirenGamitDcs22@users.noreply.github.com>
2021-12-23 11:32:03 +0530

    Update Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState, useEffect} from 'react'
+import { BrowserRouter as Router,Route,Routes } from 'react-router-dom'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
+import About from './components/About'
+import Context from "./component/Context";
+import {ShowContext} from "./component/Context";
 
 function App() {
+  const [showAddTask, setShowAddTask]=useState(false)
+  const [tasks,setTasks] =useState([])
+
+  useEffect(()=>{
+    const getTask=async()=>{
+      const res=await featchTasks();
+      setTasks(res)
+    }
+    getTask();
+  },[])
+  //fetch tasks
+  const featchTasks=async()=>{
+    const res=await fetch("http://localhost:5000/tasks");
+    const data=await res.json();
+    return data;
+  }
+
+  //Add task
+  const addTask =async(task)=>{
+    const res=await fetch("http://localhost:5000/tasks",{
+      method: "POST",
+      headers:{
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(task)
+    });
+      const newTask= await res.json();
+      setTasks([...tasks,newTask])
+  }
+  //Delete A Task
+  const DeleteTask =async(id)=>{
+      await fetch(`http://localhost:5000/tasks/${id}`,{
+        method: 'DELETE'
+      });
+      setTasks(tasks.filter((task)=>task.id !== id))
+  }
+
+  const featchTask=async(id)=>{
+    const res=await fetch(`http://localhost:5000/tasks/${id}`);
+    const data=await res.json();
+    return data;
+  }
+  //Toggle
+  const toggleReminder =async(id)=>{
+    const taskToToggle=await featchTask(id)
+    const updateTask ={...taskToToggle,reminder:!taskToToggle.reminder}
+    const res=await fetch(`http://localhost:5000/tasks/${id}`,{
+      method: 'PUT',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify(updateTask)
+    })
+    const data = await res.json();
+    setTasks(tasks.map((task)=>task.id === id ?{...task,reminder:data.reminder} : task))
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div className='container'>
+      <ShowContext.Provider value={showAddTask}>
+          <Header 
+            onAdd={() => setShowAddTask(showAddTask)} 
+          />
+        </ShowContext.Provider>
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <>
+                {showAddTask && <AddTask onAdd={addTask} />}
+                {tasks.length > 0 ? (
+                  <Context.Provider value={tasks}>
+                    <Tasks
+                      onDelete={delTask}
+                      onToggle={toggleReminder}
+                      tasks={tasks}
+                    />
+                  </Context.Provider>
+                ) : (
+                  'No Tasks To Show'
+                )}
+              </>
+            }
+          />
+          <Route path='/about' element={<About />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import Button from './Button';
+import {useLocation} from 'react-router-dom';
+const Header = ({title,onAdd,showAdd}) => {
+    const location=useLocation();
+    return (
+        <header className='header' style={{color: 'red',backgroundColor: 'black',padding:20}}>
+            <h1 >{title}</h1>
+            {location.pathname==="/" &&<Button color={showAdd?'red':'green'} text={showAdd?'Close':'Add'} onClick={onAdd}/>}
+        </header>
+    )
+}
+Header.defaultProps = {
+    title: 'Task Tracker',
+}
+export default Header
